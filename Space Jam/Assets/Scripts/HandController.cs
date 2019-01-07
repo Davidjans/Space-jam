41ab90e2124a6e80a95a589a6594c42471b19a7d
Unknown <davidjejans@hotmail.nl>
2019-01-07 13:32:30 +0100

    Project Import

---
+++ Space Jam/Assets/Scripts/HandController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandController : MonoBehaviour {
+    private Valve.VR.EVRButtonId m_GripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+
+    private Valve.VR.EVRButtonId m_TriggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private Valve.VR.EVRButtonId m_TouchpadButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+	HashSet<InteractableItem> m_ObjectsHoveringOver = new HashSet<InteractableItem>();
+
+	private InteractableItem m_ClosestItem;
+	private InteractableItem m_InteractingItem;
+
+    private SteamVR_Controller.Device m_Controller {
+        get{ return steam.Input((int)m_TrackedObject.index);}
+    }
+    private SteamVR_TrackedObject m_TrackedObject;
+
+	void Start () {
+        m_TrackedObject = GetComponent<SteamVR_TrackedObject>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_Controller == null){
+            Debug.Log("Wheres yo controller my dude");
+            return;
+        }
+
+        if (m_Controller.GetPressDown(m_TriggerButton))
+        {
+			float minDistance = float.MaxValue;
+			float distance;
+			foreach (InteractableItem item in m_ObjectsHoveringOver)
+			{
+				distance = (item.transform.position - transform.position).sqrMagnitude;
+				if (distance < minDistance)
+				{
+					minDistance = distance;
+					m_ClosestItem = item;
+				}
+			}
+			m_InteractingItem = m_ClosestItem;
+
+			if (m_InteractingItem)
+			{
+				if (m_InteractingItem.IsInteracting())
+				{
+					m_InteractingItem.EndInteraction(this);
+				}
+				m_InteractingItem.BeginInteraction(this);
+			}
+		}
+        if (m_Controller.GetPressUp(m_TriggerButton))
+        {
+			m_InteractingItem.EndInteraction(this);
+		}
+
+        if (m_Controller.GetPressDown(m_GripButton))
+        {
+			
+
+		}
+        if (m_Controller.GetPressUp(m_GripButton))
+        {
+			
+		}
+
+
+        if (m_Controller.GetPressDown(m_TouchpadButton))
+        {
+			FindObjectOfType<CrateBehaviour>().m_CheckWhatPassed();
+        }
+        if (m_Controller.GetPressUp(m_TouchpadButton))
+        {
+        }
+    }
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+		if (collidedItem)
+		{
+			m_ObjectsHoveringOver.Add(collidedItem);
+		}
+	}
+	private void OnTriggerExit(Collider collider)
+	{
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+		if (collidedItem)
+		{
+			m_ObjectsHoveringOver.Remove(collidedItem);
+		}
+	}
+}
---
+++ Space Jam/Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+	public Rigidbody m_RigidBody;
+
+	private bool m_CurrentlyInteracting;
+
+	private float m_VelocityFactor = 20000f;
+	private Vector3 m_PosDelta;
+
+	private float m_RotationFactor = 400f;
+	private Quaternion m_RotationDelta;
+	private float m_Angle;
+	private Vector3	m_Axis;
+
+	private HandController m_AttachedHand;
+
+	private Transform m_InteractionPoint;
+
+	private bool m_BeingThrown;
+	private Vector3 m_ThrownPosition;
+	public float m_ThrownDistance;
+	private void Start()
+	{
+		m_RigidBody = GetComponent<Rigidbody>();
+		m_InteractionPoint = new GameObject().transform;
+		m_VelocityFactor /= m_RigidBody.mass;
+		m_RotationFactor /= m_RigidBody.mass;
+	}
+
+	private void Update()
+	{
+		if(m_AttachedHand && m_CurrentlyInteracting)
+		{
+			m_PosDelta = m_AttachedHand.transform.position - m_InteractionPoint.position;
+			m_RigidBody.velocity = m_PosDelta * m_VelocityFactor * Time.fixedDeltaTime;
+
+			m_RotationDelta = m_AttachedHand.transform.rotation * Quaternion.Inverse(m_InteractionPoint.rotation);
+			m_RotationDelta.ToAngleAxis(out m_Angle, out m_Axis);
+
+			if(m_Angle > 180)
+			{
+				m_Angle -= 360;
+			}
+
+			m_RigidBody.angularVelocity = (Time.fixedDeltaTime * m_Angle * m_Axis) * m_RotationFactor;
+		}
+	}
+
+
+	public void BeginInteraction(HandController hand)
+	{
+		m_AttachedHand = hand;
+		m_InteractionPoint.position = hand.transform.position;
+		m_InteractionPoint.rotation = hand.transform.rotation;
+		m_InteractionPoint.SetParent(transform, true);
+		Debug.Log("Interaction Started");
+		m_CurrentlyInteracting = true;
+	}
+
+	public void EndInteraction(HandController hand)
+	{
+		Debug.Log("Interaction Ended");
+		if ( hand == m_AttachedHand)
+		{
+			m_AttachedHand = null;
+			m_CurrentlyInteracting = false;
+			m_BeingThrown = true;
+			m_ThrownPosition = transform.position;
+		}
+	}
+
+	public bool IsInteracting()
+	{
+		return m_CurrentlyInteracting;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(m_BeingThrown == true)
+		{
+			m_ThrownDistance = Vector3.Distance(m_ThrownPosition, transform.position);
+			m_BeingThrown = false;
+		}
+	}
+}
