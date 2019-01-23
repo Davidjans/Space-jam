f307a3ff489838f2691313cfe0016204e488dc6e
Unknown <davidjejans@hotmail.nl>
2019-01-23 12:10:36 +0100

    most basic form of moose guide

---
+++ Space Jam/Assets/Scripts/BasketMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketMovement : MonoBehaviour
+{
+	[SerializeField] private float m_MovementSpeed;
+	[SerializeField] private Transform m_BoardTransform;
+	[SerializeField] private Vector3 MaxPositions;
+	[SerializeField] private Vector3 MinPositions;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.I) && m_BoardTransform.position.y <= MaxPositions.y)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x, m_BoardTransform.position.y + m_MovementSpeed, m_BoardTransform.position.z);
+		}
+		if (Input.GetKeyDown(KeyCode.K) && m_BoardTransform.position.y >= MinPositions.y)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x, m_BoardTransform.position.y - m_MovementSpeed, m_BoardTransform.position.z);
+		}
+		if (Input.GetKeyDown(KeyCode.L) && m_BoardTransform.position.x >= MinPositions.x)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x - m_MovementSpeed, m_BoardTransform.position.y , m_BoardTransform.position.z);
+		}
+		if (Input.GetKeyDown(KeyCode.J) && m_BoardTransform.position.x <= MaxPositions.x)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x + m_MovementSpeed, m_BoardTransform.position.y, m_BoardTransform.position.z);
+		}
+		if (Input.GetKeyDown(KeyCode.N) && m_BoardTransform.position.z >= MinPositions.z)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x, m_BoardTransform.position.y, m_BoardTransform.position.z - m_MovementSpeed);
+		}
+		if (Input.GetKeyDown(KeyCode.M) && m_BoardTransform.position.z <= MaxPositions.z)
+		{
+			m_BoardTransform.position = new Vector3(m_BoardTransform.position.x , m_BoardTransform.position.y, m_BoardTransform.position.z + m_MovementSpeed);
+		}
+	}
+}
---
+++ Space Jam/Assets/Scripts/Score.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+	[SerializeField] private float m_OriginalCheckTimer;
+	private bool m_CheckOnePassed;
+	private bool m_CheckTwoPassed;
+	private bool m_CheckThreePassed;
+	public float m_CheckTimer;
+	public float m_Score;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		m_CheckTimer = m_OriginalCheckTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_CheckThreePassed == true)
+		{
+			m_Score++;
+			m_CheckOnePassed = false;
+			m_CheckTwoPassed = false;
+			m_CheckThreePassed = false;
+			m_CheckTimer = m_OriginalCheckTimer;
+		}
+		if (m_CheckOnePassed)
+		{
+			m_CheckTimer -= Time.deltaTime;
+		}
+		if(m_CheckTimer <= 0)
+		{
+			m_CheckTimer = m_OriginalCheckTimer;
+			m_CheckOnePassed = false;
+			m_CheckTwoPassed = false;
+			m_CheckThreePassed = false;
+		}
+    }
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("CheckOne"))
+		{
+			m_CheckOnePassed = true;
+		}
+		if (other.CompareTag("CheckTwo") && m_CheckOnePassed)
+		{
+			m_CheckTwoPassed = true;
+		}
+		if (other.CompareTag("CheckThree") && m_CheckTwoPassed)
+		{
+			m_CheckThreePassed = true;
+		}
+	}
+}
